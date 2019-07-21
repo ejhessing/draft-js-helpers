ce016c91a3fbb634eb3281183f35dbd30a1e0c8d
Erwin Hessing <ejhessing@gmail.com>
2019-07-22 11:31:42 +1200

    Update Exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import { getSelected } from "./selected";
+import { getSelected, forceSelection, getSelectedText } from "./selected";
 import { getPosition } from "./position";
 import { setStyles } from './styles';
+import { insertImage } from './image';
+import { getBlockType, toggleBlockStyle } from './blockType';
+import { addInlineStyle, removeInlineStyle, toggleInlineStyle } from './inlineStyles';
+import { getLinkDetails, insetLine, removeLink } from './hyperlink';
 
-
-export { getSelected, getPosition, setStyles };
+export { addInlineStyle, getBlockType, getLinkDetails, getSelected, getSelectedText, getPosition, insertImage, insetLine, setStyles, forceSelection, removeInlineStyle, removeLink, toggleInlineStyle, toggleBlockStyle, };
